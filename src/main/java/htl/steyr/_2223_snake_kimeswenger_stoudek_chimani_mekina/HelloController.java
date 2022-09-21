b9a0b7db2ec17598577ccd95cdca75eeae3910ea
nmekina <95867724+nmekina@users.noreply.github.com>
2022-09-21 08:38:54 +0200

    Initial commit

---
+++ src/main/java/htl/steyr/_2223_snake_kimeswenger_stoudek_chimani_mekina/HelloController.java
@@ -0,0 +1,14 @@
+package htl.steyr._2223_snake_kimeswenger_stoudek_chimani_mekina;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module htl.steyr._2223_snake_kimeswenger_stoudek_chimani_mekina {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens htl.steyr._2223_snake_kimeswenger_stoudek_chimani_mekina to javafx.fxml;
+    exports htl.steyr._2223_snake_kimeswenger_stoudek_chimani_mekina;
+}
