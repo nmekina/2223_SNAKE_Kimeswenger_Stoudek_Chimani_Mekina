c0e7bef4e1f9f4471b4536a3fd1e6acc9073b6f9
Christian42 <cchimani@htl-steyr.ac.at>
2022-09-21 10:28:37 +0200

    gson

--- src/main/java/htl/steyr/_2223_snake_kimeswenger_stoudek_chimani_mekina/Controller/StartmenueController.java
+++ src/main/java/htl/steyr/_2223_snake_kimeswenger_stoudek_chimani_mekina/Controller/StartmenueController.java
@@ -10,6 +10,11 @@ import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.jar.Attributes;
 
@@ -19,17 +24,33 @@ public class StartmenueController {
     public ColorPicker colourpicker;
 
     ArrayList<String> list1=new ArrayList<>();
-    ObservableList<String>list;
+    ObservableList<Integer> list = FXCollections.observableArrayList(1, 2, 3);
 
     public ChoiceBox savedplayersc;
 
-    /*public void initialize() {
-        savedplayersc.setItems( list);
+    public void initialize() {
 
+        for (String oneplayer : allplayers) {
+            choicebox_username.getItems().add(oneplayer);
+        }
+        addSpieler();
 
-    }*/
+    }
 
     public void settingsbuttonclicked(ActionEvent actionEvent) {
-        list.add(nameinput.getText());
+        list1.add(nameinput.getText());
+    }
+    public void addSpieler() throws IOException {
+        File file = new File("src/main/java/htl/steyr/_2223_snake_kimeswenger_stoudek_chimani_mekina/Model/highscore.json");
+        String content = new String(Files.readAllBytes(Paths.get(file.toURI())));
+        JSONArray json = new JSONArray(content);
+
+
+        for (int z = 0; z < json.length(); z++) {
+            JSONObject getplayer = json.getJSONObject(z);
+            String player = getplayer.getString("name");
+            allplayers.add(player);
+        }
     }
 }
+
