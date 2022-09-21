58ed9153933ba8dbaf5db3fa0cd1423ffdd5c470
skimeswe <skimeswe@htl-steyr.ac.at>
2022-09-21 10:30:49 +0200

    Musik Kahoot

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module htl.steyr._2223_snake_kimeswenger_stoudek_chimani_mekina {
     requires javafx.controls;
     requires javafx.fxml;
+    requires soundPlay;
 
 
     opens htl.steyr._2223_snake_kimeswenger_stoudek_chimani_mekina to javafx.fxml;
