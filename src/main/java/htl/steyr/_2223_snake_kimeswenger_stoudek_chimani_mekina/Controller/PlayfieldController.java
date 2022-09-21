9ceb231f8686f00bc958d0454e5d22943f898831
lstoudek <leonhard.stoudek@gmail.com>
2022-09-21 10:48:56 +0200

    Playfield files

--- src/main/java/htl/steyr/_2223_snake_kimeswenger_stoudek_chimani_mekina/Controller/PlayfieldController.java
+++ src/main/java/htl/steyr/_2223_snake_kimeswenger_stoudek_chimani_mekina/Controller/PlayfieldController.java
@@ -1,44 +1,49 @@
 package htl.steyr._2223_snake_kimeswenger_stoudek_chimani_mekina.Controller;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
-import java.util.EventListener;
 
 
 public class PlayfieldController {
 
-    private static final int ROW_NR = 20;
-    private static final int COL_NR = 25;
     @FXML
-    GridPane playfield = new GridPane();
+    public GridPane playfield = new GridPane();
 
-    /**@author lstoudek
-     *  initialisiert das Spielfeld
-     */
-    public PlayfieldController(){
-        for (int i = 0; i < ROW_NR; i++){
-            for (int j = 0; j < COL_NR; j++){
-                Pane pane = new Pane();
-                pane.setStyle("-fx-border-color: black; -fx-border-width: 2px");
-                playfield.add(pane, i, j);
-                System.out.println(pane + "\t" + i + "\t" + j);
-                playfield.setOnMouseClicked(myEventhandler);
+
+
+
+    /*
+ if (playfield != null) {
+            for (int i = 0; i < ROW_NR; i++) {
+                for (int j = 0; j < COL_NR; j++) {
+                    Pane pane = new Pane();
+                    pane.setPrefHeight(20);
+                    pane.setPrefWidth(20);
+                    pane.setStyle("-fx-border-color: black; -fx-border-width: 2px;");
+                    playfield.add(pane, i, j);
+                    System.out.println(pane + "\t" + i + "\t" + j);
+                }
             }
+            ColumnConstraints col1 = new ColumnConstraints();
+            col1.setPrefWidth(20);
+            playfield.getColumnConstraints().addAll(col1);
+            System.out.println("aef");
         }
-        System.out.println("aef");
+     */
+    /**@author lstoudek
+     *
+     */
+    public PlayfieldController() {
+        Pane pane = new Pane();
+        pane.setMaxHeight(1);
+        pane.setMaxWidth(1);
+        pane.getChildren().add(new Label("seffe"));
+        playfield.add(pane, 0, 0);
     }
-
-    EventHandler<MouseEvent> myEventhandler = new EventHandler<MouseEvent>(){
-
-        @Override
-        public void handle(MouseEvent mouseEvent) {
-            System.out.println("asdf");
-        }
-    };
 
 
 }
